aa32b2490a2015f8264d83c53afdfd743a81cf94
RealDzolat <dzolbusines@gmail.com>
2026-08-12 21:44:36 +0200

    Added Navbar v1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Button } from "@/components/ui/button";
+import { Home, FolderOpen } from "lucide-react";
 
 export default function Navbar() {
-    return (
-        <nav className="fixed flex justify-between items-center w-full h-15 bg-gray-900">
-            <Image
-                className="mx-5"
-                width={40}
-                height={40}
-                src="/DIO.png"
-                alt="Profile Picture"
-            />
+  return (
+    <nav className="fixed flex justify-around items-center w-full h-15 bg-black/20 backdrop-blur-xl border-b border-white/10 shadow-lg">
+      <Image
+        className="rounded-full"
+        width={40}
+        height={40}
+        src="/DIO.png"
+        alt="Profile Picture"
+      />
 
-            <div className="flex justify-center items-center h-15">
-                <Link href="/">Home</Link>
-            </div>
+      <ButtonGroup>
+        <Button variant="outline" render={<Link href="/" />}>
+          <Home data-icon="inline-start" />
+          Home
+        </Button>
 
-            <Link href="/contact" className="mx-5">
-                Contact
-            </Link>
-        </nav>
-    );
+        <Button variant="outline" render={<Link href="/projects" />}>
+          <FolderOpen data-icon="inline-start" />
+          Projects
+        </Button>
+      </ButtonGroup>
+
+      <Button
+        render={<Link href="/contact" />}
+        className="
+        rounded-full
+        bg-gradient-to-r from-[#FF0000] to-[#FDCF58]
+        text-white
+        border-0
+        shadow-[0_0_20px_rgba(253,207,88,0.45)]
+        transition-shadow">
+        Contact
+      </Button>
+    </nav>
+  );
 }
