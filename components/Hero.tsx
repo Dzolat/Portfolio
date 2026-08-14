15cd4aac667b8b3748ac97e218a2bfdc74420df4
RealDzolat <dzolbusines@gmail.com>
2026-08-14 11:05:45 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { FaGithub } from "react-icons/fa";
+import { FolderOpen } from "lucide-react";
 
 export default function Hero() {
   return (
     <main className="min-h-screen pt-24">
-      <section className="max-w-7xl mx-auto px-8 py-16 flex items-center gap-20">
+      <section className="max-w-7xl mx-auto px-8 pt-16 pb-4 flex items-center gap-20">
         {/* Left card */}
         <div className="shrink-0">
           <div className="w-[320px] rounded-3xl bg-white p-5">
@@ -17,12 +21,7 @@ export default function Hero() {
               />
             </div>
 
-            <h2 className="mt-5 text-3xl font-bold text-black">Alan Stec</h2>
-
-            <h2 className="text-1xl font-bold text-gray-900">
-              @RealDzolat
-            </h2>
-
+            <h2 className="mt-5 text-3xl font-bold text-gray-900">@RealDzolat</h2>
             <p className="text-black/60">Developer</p>
           </div>
         </div>
@@ -41,16 +40,21 @@ export default function Hero() {
             I build software, web apps and games.
           </p>
 
-          <div className="flex gap-3 mt-8">
-            <span className="pill">C++</span>
-
-            <span className="pill">C</span>
-
-            <span className="pill">C#</span>
-
-            <span className="pill">Python</span>
+          <div className="flex gap-3 mt-4">
+            <Image width="40" height="40" src="/logos/C++.png" alt="C++" />
+            <Image width="40" height="40" src="/logos/C.png" alt="C" />
           </div>
         </div>
+      </section>
+      <section className="max-w-7xl mx-auto px-8 flex flex-center items-center gap-12">
+        <Button className="rounded-full" render={<a href="https://github.com/Dzolat" />}>
+          <FaGithub data-icon="inline-start" />
+          View Github
+        </Button>
+        <Button className="rounded-full" render={<Link href="/projects" />}>
+          <FolderOpen data-icon="inline-start" />
+          Explore Projects
+        </Button>
       </section>
     </main>
   );
