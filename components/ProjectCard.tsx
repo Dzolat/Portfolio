03f100f0528872ae7f8790e60eae2c3fd631a134
RealDzolat <dzolbusines@gmail.com>
2026-08-14 10:55:12 +0200

    Created ProjectCard

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ProjectCardProps } from "@/data/projects";
+import Link from "next/link";
+
+export default function ProjectCard({
+    title,
+    description,
+    path
+}: ProjectCardProps) {
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+            <CardAction>
+                <Button render={<Link href={`/projects/${path}`} />} className="rounded-full">View more {`->`}</Button>
+            </CardAction>
+        </CardHeader>
+        <CardContent>
+            <Image src={`/projects/${path}.png`} alt={title} width={1200} height={700} className="w-full rounded-lg object-cover" />
+        </CardContent>
+    </Card>
+  );
+}
