c71a19a7f82e943c0712ae07e538af5e450e67ec
RealDzolat <dzolbusines@gmail.com>
2026-08-14 10:55:50 +0200

    Created projects

---
+++ components/Projects.tsx
@@ -0,0 +1,18 @@
+import ProjectCard from "./ProjectCard";
+import { projects } from "@/data/projects";
+
+export default function Projects() {
+  return (
+    <main className="min-h-screen pt-24">
+      <section className="max-w-7xl mx-auto px-8 pt-16 pb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => (
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            path={project.path}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
---
+++ data/projects.ts
@@ -0,0 +1,13 @@
+export const projects: ProjectCardProps[] = [
+    {
+        title: "Celestial Core Website",
+        description: "Placeholder",
+        path: "main_website",
+    },
+]
+
+export type ProjectCardProps = {
+    title: string,
+    description: string,
+    path: string,
+}
