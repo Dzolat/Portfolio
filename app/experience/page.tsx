a34ce9df04e21af9ae7a8708fc03ba9b8b32dd2f
RealDzolat <dzolbusines@gmail.com>
2026-08-20 00:30:20 +0200

    Create /experience

---
+++ app/experience/page.tsx
@@ -0,0 +1,10 @@
+import Certifications from "@/components/Certifications";
+
+export default function ExperiencePage()
+{
+    return (
+        <main className="min-h-screen px-6 pb-20 pt-28 sm:px-10 sm:pt-32 lg:px-20">
+            <Certifications />
+        </main>
+    )
+}
