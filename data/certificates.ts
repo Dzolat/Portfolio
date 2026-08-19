e80789d99b2dff65b659ba1e8038b00781c6224f
RealDzolat <dzolbusines@gmail.com>
2026-08-20 00:27:15 +0200

    Create certificates.ts

---
+++ data/certificates.ts
@@ -0,0 +1,13 @@
+export const certificates: CertificateProps[] = [
+    {
+        title: "Google AI Essentials",
+        link: "https://www.coursera.org/account/accomplishments/specialization/MZ9R5ROJ9HZX",
+        path: "google_ai_essentials.png",
+    },
+]
+
+export type CertificateProps = {
+    title: string,
+    link: string,
+    path: string,
+}
