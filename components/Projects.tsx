9636be5707888de90d38149046b8822b8138f951
RealDzolat <dzolbusines@gmail.com>
2026-08-20 00:27:23 +0200

    Fix bug

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,6 +7,7 @@ export default function Projects() {
       <section className="max-w-7xl mx-auto px-8 pt-16 pb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project) => (
           <ProjectCard
+            key={project.path}
             title={project.title}
             description={project.description}
             path={project.path}
